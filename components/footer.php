38b6ed8a7511cadf09fbf1a64aea665b5a482920
JasmineIbrahim <137914855+JasmineIbrahim@users.noreply.github.com>
2024-06-29 21:04:52 +0300

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,13 @@
+<section class="footer">
+    <div class="icons">
+        <a href="https://www.facebook.com/IEEEYUWIEAG?mibextid=ZbWKwL" class="fa fa-facebook"></a>
+        <a href="https://instagram.com/ieee_yu_wie?igshid=MzRlODBiNWFlZA==" class="fa fa-instagram"></a>
+        <a href="https://www.linkedin.com/in/women-in-engineering-yu-860a22292?trk=contact-info&fbclid=IwAR30Atjx9qAm0P831nQA93Dslzq8AvJNEscC1xoLYVHtd_V3ygqswFz5dnU" class="fa fa-linkedin"></a>
+        <a href="mailto:ieee.wie.yu@gmail.com" class="fa fa-envelope"></a>
+    </div>
+
+    <div class="credit">
+        Copyright <i class="fa fa-copyright"></i> <span>2024 Women In Engineering Affinty Group-Yarmouk University </span> | All
+        Rights Reserved
+    </div>
+</section>
---
+++ components/header.php
@@ -0,0 +1,20 @@
+<header>
+    <nav>
+        <a href="home.php"><img src="img/logo.ico"></a>
+        <div class="nav-links" id="navlinks">
+            <i class="fa fa-times" onclick="hideMenu()"></i>
+            <ul>
+                <li><a href="home.php">HOME</a></li>
+                <li><a href="about.php">ABOUT</a></li>
+                <li><a href="events.php">EVENTS</a></li>
+                <li><a href="news.php">NEWS</a></li>
+                <li><a href="hall.php">HALL OF FAME</a></li>
+                <!--<li><a href="gallery.php">GALLERY</a></li>-->
+                <li><a href="home.php#contact">CONTACT</a></li>
+            </ul>
+       </div>
+        <!--<i class="fa fa-moon-o" style="display:block; cursor:pointer;" onclick="dark()"></i>-->
+        <a href="login.php" style="color:var(--dark); text-decoration:none;"><i class="fa fa-user" style="display:block;"></i></a>
+        <i class="fa fa-bars" onclick="showMenu()"></i>
+    </nav>
+</header>
